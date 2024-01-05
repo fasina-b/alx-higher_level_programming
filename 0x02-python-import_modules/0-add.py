4cbd0223b3b5ee548830a359c2d8788d13ef95ce
Boluwatife Fasina <boluayofashina@gmail.com>
2024-01-04 18:14:28 -0800

    ffff

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
-#!/usr/bin/python3.8.5
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
