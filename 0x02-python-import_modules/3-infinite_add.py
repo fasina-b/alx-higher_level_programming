e8b10273b2866b0c8091ea372d81831d499b71de
fasina.b <boluayofashina@gmail.com>
2024-01-05 03:30:27 +0100

    Mandatory Tasks Completed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import sys
-arg0 = sys.argv[1:]
-ans = sum(map(int, arg0))
-print(ans)
+if __name__ == "__main__":
+    import sys
+    arg0 = sys.argv[1:]
+    ans = sum(map(int, arg0))
+    print(ans)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4 as hidden
-chars = dir(hidden)
-chars.sort()
-for char in chars:
-    if not char.startswith("__"):
-        print(char)
+if __name__ == "__main__":
+    import hidden_4 as hidden
+    chars = dir(hidden)
+    chars.sort()
+    for char in chars:
+        if not char.startswith("__"):
+            print(char)
