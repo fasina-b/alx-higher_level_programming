ca338f41fe1d50c013d268b435478d7052de46d1
fasina.b <boluayofashina@gmail.com>
2024-01-05 03:18:14 +0100

    new

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv) - 1
-opp = "argument" if argc == 1 else "arguments"
-char = ":" if argc else "."
-print("{} {}{}".format(argc, opp, char))
-for i in range(1, argc + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+ import sys
+ argc = len(sys.argv) - 1
+ opp = "argument" if argc == 1 else "arguments"
+ char = ":" if argc else "."
+ print("{} {}{}".format(argc, opp, char))
+ for i in range(1, argc + 1):
+     print("{}: {}".format(i, sys.argv[i]))
