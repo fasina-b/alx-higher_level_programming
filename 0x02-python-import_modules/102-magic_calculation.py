43af484671bfd7e25531383427cddc2ca9c35da8
fasina.b <boluayofashina@gmail.com>
2024-01-11 01:51:10 +0100

    Advanced

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        summed = add(a, b)
+        for i in range(4, 6):
+            summed = add(c, i)
+        return summed
+    else:
+        return sub(a, b)
