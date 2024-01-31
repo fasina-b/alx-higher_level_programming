80db24e488629730747c903615a0743877541e11
Boluwatife Fasina <boluayofashina@gmail.com>
2024-01-30 18:41:03 -0800

    sssss

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Module to divide all elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Divide all elements of a matrix.
+
+    Args:
+        matrix (list of lists): Matrix of integers or floats.
+        div (number): Number (integer or float) to divide the matrix by.
+
+    Returns:
+        list of lists: New matrix with elements divided by div.
+
+    Raises:
+        TypeError: If matrix is not a list of lists of integers/floats.
+        TypeError: If each row of the matrix is not of the same size.
+        TypeError: If div is not a number (integer or float).
+        ZeroDivisionError: If div is equal to 0.
+    """
+    if not isinstance(matrix, list)
+    or not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(isinstance(num, (int, float)) for row in matrix for num in row):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(num / div, 2) for num in row] for row in matrix]
---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints My name is <first name> <last name>.
+
+    Args:
+        first_name (str): The first name.
+        last_name (str, optional): The last name. Defaults to "".
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    full_name = "My name is"
+    if last_name:
+        full_name += " {} {}".format(first_name, last_name)
+    else:
+        full_name += " {}".format(first_name)
+
+    print(full_name)
