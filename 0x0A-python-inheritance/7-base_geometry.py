7aa91a43b83ca7bfc48b05da7787a1998ca65115
Boluwatife Fasina <boluayofashina@gmail.com>
2024-02-05 15:47:33 -0800

    7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
             NotImplementedError: If the method is
             not implemented in the derived class.
         """
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validates a parameter as an integer.
@@ -25,7 +25,7 @@ class BaseGeometry:
             TypeError: If the value is not an integer.
             ValueError: If the value is not greater than 0.
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@ Defines a Rectangle class that inherits from BaseGeometry.
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
     Represents a rectangle and inherits from the BaseGeometry class.
