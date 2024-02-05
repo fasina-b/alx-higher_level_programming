70f660601ab236ab8a29cb6deb1f5d20234d5da5
Boluwatife Fasina <boluayofashina@gmail.com>
2024-02-05 15:54:26 -0800

    8

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+from 9-rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class, a special case of a rectangle with equal sides."""
+
+    def __init__(self, size):
+        """
+        Initializes a square with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            ValueError: If size is not a positive integer.
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Calculates and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Defines a Square class that inherits from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Represents a Square, a special case of a Rectangle.
+    """
+
+    def __init__(self, size: int):
+        """
+        Initializes a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size: int = size
