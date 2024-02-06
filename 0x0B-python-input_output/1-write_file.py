809d9a5cc15169209530aa110b6aa26f8b59f0f4
Boluwatife Fasina <boluayofashina@gmail.com>
2024-02-06 15:22:05 -0800

    5

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -11,10 +11,7 @@ def write_file(filename="", text=""):
     Args:
         filename (str): The name of the file.
         text (str): The text to write to the file.
-
-    Raises:
-        Exception: If the file cannot be opened.
     """
 
     with open(filename, 'w', encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
