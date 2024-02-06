9d74e6dc86d4abbbdcc5f4aece69c799a7f9ea26
Boluwatife Fasina <boluayofashina@gmail.com>
2024-02-06 14:50:27 -0800

    1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 
+A module containing a function to read text from a file.
+"""
+
+
+def read_text_from_file(filename=""):
+    """ 
+    Read text from a file and print its contents.
+
+    Args:
+        filename (str): The name of the file to read from.
+
+    Raises:
+        FileNotFoundError: If the specified file cannot be found.
+        PermissionError: If the specified file cannot be opened due to permission issues.
+    """
+    try:
+        with open(filename, 'r', encoding="utf-8") as file:
+            read_data = file.read()
+            print(read_data, end='')
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except PermissionError:
+        print(f"Error: Permission denied while trying to open '{filename}'.")
