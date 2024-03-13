7d9dad9b8e5b8f55b41df489d68a5c363883c1cc
Boluwatife Fasina <boluayofashina@gmail.com>
2024-03-12 19:38:17 -0700

    0-4

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- This displays the three cities with highest avg temps between July and August.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
