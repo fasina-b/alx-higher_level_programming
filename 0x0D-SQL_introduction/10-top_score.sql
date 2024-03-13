9441541f54f1019ee975e94f79b7d8e48d8c82bd
Boluwatife Fasina <boluayofashina@gmail.com>
2024-03-12 19:27:58 -0700

    0-2

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,2 @@
+-- This lists all records of second_table by score order
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- This lists all records in the second_table with a score >= 10 in my MySQL server.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
