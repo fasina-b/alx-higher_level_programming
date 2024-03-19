a11d923d1b43ef8d289898fe83893927eafa3d1c
Boluwatife Fasina <boluayofashina@gmail.com>
2024-03-18 19:21:51 -0700

    Done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
--- Create user user_0d_1 with all privileges and set password
+-- Create user user_0d_1 with all privileges and set password                                                                              
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON . TO 'user_0d_1'@'localhost';
+-- Grant all privileges to user_0d_1                                                                                                       
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
