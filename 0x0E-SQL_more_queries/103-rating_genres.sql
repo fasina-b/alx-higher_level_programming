14c952b9b3fc807854a9dfc9c8df6fd187bb5856
Boluwatife Fasina <boluayofashina@gmail.com>
2024-03-18 19:12:07 -0700

    Done

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,8 @@
+-- List all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Records are ordered by descending rating.
+SELECT g.name, SUM(r.rate) AS rating
+FROM tv_genres AS g
+INNER JOIN tv_show_genres AS s ON s.genre_id = g.id
+INNER JOIN tv_show_ratings AS r ON r.show_id = s.show_id
+GROUP BY g.name
+ORDER BY rating DESC;
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user user_0d_2 if it does not exist and set password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant only SELECT privilege to user_0d_2 on database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
